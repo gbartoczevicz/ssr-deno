3a0ec97bde92d6080e1a27d715e996947f6c2891
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-10-17 15:39:31 -0300

    update main

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 import { listenAndServe } from "std/http/mod.ts";
 import { makeResponse } from "~/server/make_response.ts";
 
-listenAndServe(":3000", async (request) => {
+const PORT = 3000;
+
+listenAndServe(`:${PORT}`, async (request) => {
   const response = await makeResponse(request);
 
   return response;
 });
+
+console.log(`The server is running at ${PORT}`);
